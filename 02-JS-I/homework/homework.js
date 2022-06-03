cb3c78d5d8828d15c1a21cc7953b22a39d1074a2
unknown <sebastian99fraa@gmail.com>
2022-06-03 14:03:37 -0300

    resolucion js1 completado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -67,7 +67,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-if (str1.lenght === str2.length) return true;
+if (str1.length === str2.length) return true;
 return false;
 
 }
@@ -135,19 +135,20 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  return math (num);
+  return Math.round(num);
+
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return math.Random()
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -157,7 +158,7 @@ function esPositivo(numero) {
   //Si el número es 0, devuelve false
   if (numero >0)return "Es positivo"
   if (numero <0)return "Es negativo"
-  return false
+  return false;
 
 }
 
@@ -179,7 +180,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "hola " + nombre + " ! "
+  return "Hola "+(nombre)+"!";
+  
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -221,7 +223,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra === 'a' || letra === 'e' ||letra === 'i' ||letra === 'o' || letra === 'u')return 'es vocal';
+  if (letra === 'a' || letra === 'e' ||letra === 'i' ||letra === 'o' || letra === 'u')return 'Es vocal';
   if (letra.lenght > 1)return "Dato incorrecto";
   return "Dato incorrecto";
 }
